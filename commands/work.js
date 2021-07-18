d2b3bc5d56b1a758ce73424c4b3a629f6974765f
Kyuno <68708488+Kyun0r@users.noreply.github.com>
2021-07-17 22:51:54 -0300

    Definindo o ms
    A dependência "ms "não estava definida.

--- commands/work.js
+++ commands/work.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const db = require("quick.db");
+const ms = require("parse-ms"); // Importando o ms
 
 module.exports.run = async (client, message, args) => {
     let user = message.author;
