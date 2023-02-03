6beeb185360e80f24d0dc7fff5d0a1775f7693de
tiagoryandev <tiagoryandev@gmail.com>
2023-02-03 19:56:00 -0300

    build: add typescript libs

--- src/client.js
+++ src/client.js
@@ -3,60 +3,90 @@ const path = require("path");
 const Discord = require("discord.js");
 const mongoose = require("mongoose");
 
+const loadEvents = require("");
 const config = require("../config.json");
 
-class DiscordApplication {
-	constructor() {
-		this.client = new Discord.Client({
-			intents: [
-				Discord.GatewayIntentBits.Guilds,
-				Discord.GatewayIntentBits.GuildMessages,
-				Discord.GatewayIntentBits.MessageContent,
-				Discord.GatewayIntentBits.GuildMembers
-			]
-		});
-    
-		this.client.commands = new Discord.Collection();
-		this.loadEvents();
-		this.loadSlashCommands();
-		this.connectDatabase();
-		this.client.login(config.clientToken);
-	}
+const client = new Discord.Client({
+	intents: [
+		Discord.GatewayIntentBits.Guilds,
+		Discord.GatewayIntentBits.GuildMessages,
+		Discord.GatewayIntentBits.MessageContent,
+		Discord.GatewayIntentBits.GuildMembers
+	]
+});
 
-	async loadEvents() {
-		const eventFolder = fs.readdirSync(path.resolve(__dirname, "events"));
+client.commands = new Discord.Collection();
 
-		for (const file of eventFolder) {
-			const event = require(`./events/${file}`);
-
-			this.client.on(event.eventType, event.execute.bind(null, this.client));
-		}
-	}
-
-	async loadSlashCommands() {
-		const folders = fs.readdirSync(path.resolve(__dirname, "commands"));
+const loadCommands = () => {
+	const folders = fs.readdirSync(path.resolve(__dirname, "commands"));
 		
-		for (const folder of folders) {
-			const commandFolder = fs.readdirSync(path.resolve(__dirname, "commands", folder));
+	for (const folder of folders) {
+		const commandFolder = fs.readdirSync(path.resolve(__dirname, "commands", folder));
 	
-			for (const file of commandFolder) {
-				const command = require(`./commands/${folder}/${file}`);
+		for (const file of commandFolder) {
+			const command = require(`./commands/${folder}/${file}`);
 			
-				this.client.commands.set(command.data.toJSON().name, command);
-			}
+			this.client.commands.set(command.data.toJSON().name, command);
 		}
 	}
+};
 
-	async connectDatabase() {
-		mongoose.Promise = global.Promise;
-		mongoose.set("strictQuery", true);
+client.login(config.client_token);
 
-		mongoose.connect(config.mongodb.url, {
-			authSource: "admin",
-			user: config.mongodb.username,
-			pass: config.mongodb.password
-		});
-	}
-}
 
-module.exports = DiscordApplication;
+
+// class DiscordApplication {
+// 	constructor() {
+// 		this.client = new Discord.Client({
+// 			intents: [
+// 				Discord.GatewayIntentBits.Guilds,
+// 				Discord.GatewayIntentBits.GuildMessages,
+// 				Discord.GatewayIntentBits.MessageContent,
+// 				Discord.GatewayIntentBits.GuildMembers
+// 			]
+// 		});
+    
+// 		this.client.commands = new Discord.Collection();
+// 		this.loadEvents();
+// 		this.loadSlashCommands();
+// 		this.connectDatabase();
+// 		this.client.login(config.clientToken);
+// 	}
+
+// 	async loadEvents() {
+// 		const eventFolder = fs.readdirSync(path.resolve(__dirname, "events"));
+
+// 		for (const file of eventFolder) {
+// 			const event = require(`./events/${file}`);
+
+// 			this.client.on(event.eventType, event.execute.bind(null, this.client));
+// 		}
+// 	}
+
+// 	async loadSlashCommands() {
+// 		const folders = fs.readdirSync(path.resolve(__dirname, "commands"));
+		
+// 		for (const folder of folders) {
+// 			const commandFolder = fs.readdirSync(path.resolve(__dirname, "commands", folder));
+	
+// 			for (const file of commandFolder) {
+// 				const command = require(`./commands/${folder}/${file}`);
+			
+// 				this.client.commands.set(command.data.toJSON().name, command);
+// 			}
+// 		}
+// 	}
+
+// 	async connectDatabase() {
+// 		mongoose.Promise = global.Promise;
+// 		mongoose.set("strictQuery", true);
+
+// 		mongoose.connect(config.mongodb.url, {
+// 			authSource: "admin",
+// 			user: config.mongodb.username,
+// 			pass: config.mongodb.password
+// 		});
+// 	}
+// }
+
+// module.exports = DiscordApplication;
