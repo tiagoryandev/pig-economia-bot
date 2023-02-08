ca1e403ee309ea5f3cfd18fd2419a373eed5d5db
tiagoryandev <tiagoryandev@gmail.com>
2023-02-07 22:45:37 -0300

    test: add test command for deploy-command

---
+++ src/commands/economy/wallet.command.ts
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from "discord.js";
+
+import { SlashCommandBase } from "../../interfaces/slash-command-base.interface";
+
+const WalletCommand: SlashCommandBase = {
+	data: new SlashCommandBuilder()
+		.setName("carteira")
+		.setDescription("💰 Veja as informações monetárias de sua carteira."),
+	execute: async (client, interaction) => {
+		const currency = Math.floor(Math.random() * 10000) + 1000;
+		const currencyFormated = currency.toLocaleString("pt-BR", {
+			style: "currency",
+			currency: "BRL",
+			minimumFractionDigits: 2
+		});
+
+		await interaction.reply({
+			content: `💰 **|** Você tem na sua cateria atualmente **${currencyFormated}**.`
+		});
+	}
+};
+
+export default WalletCommand;
