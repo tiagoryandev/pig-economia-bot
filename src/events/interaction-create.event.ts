f2bf983b0a8b6845ce00eeb6d1f704e959865dc6
tiagoryandev <tiagoryandev@gmail.com>
2023-02-07 22:58:45 -0300

    feat: add interaction command event

---
+++ src/events/interaction-create.event.ts
@@ -0,0 +1,16 @@
+import { EventBase } from "../interfaces/event-base.interface";
+
+const ReadyEvent: EventBase<"interactionCreate"> = {
+	eventType: "interactionCreate",
+	execute: async (client, interaction) => {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = client.commands.get(interaction.commandName);
+
+		if (!command) return;
+
+		command.execute(client, interaction);
+	}
+};
+
+export default ReadyEvent;
