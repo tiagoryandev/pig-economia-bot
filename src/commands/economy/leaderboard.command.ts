bf9aa780e8a3c191e3a761ccc251b180c5730b2f
tiagoryandev <tiagoryandev@gmail.com>
2023-02-09 21:14:49 -0300

    feat: add leaderboard command

---
+++ src/commands/economy/leaderboard.command.ts
@@ -0,0 +1,31 @@
+import { SlashCommandBuilder } from "discord.js";
+
+import prisma from "../../libs/prisma";
+import { SlashCommandBase } from "../../interfaces/slash-command-base.interface";
+import { formatCurrency } from "../../helpers/format-values";
+
+const WalletCommand: SlashCommandBase = {
+	data: new SlashCommandBuilder()
+		.setName("rank")
+		.setDescription("💰 Veja os usuários mais ricos."),
+	execute: async (client, interaction) => {
+		const usersList = await prisma.users.findMany({
+			take: 10
+		});
+
+		const usersSortted = usersList.sort((a, b) => a.wallet + a.bank < b.wallet + b.bank ? 1 : -1);
+
+		await interaction.reply({
+			content: [
+				`:bank: **|** ${interaction.user.toString()}, veja a lista dos usuários mais ricos:`,
+				...usersSortted.map((user, index) => {
+					const currency = formatCurrency(Number(user.wallet + user.bank) / 100);
+	
+					return `> \`${index + 1}º\` **| ${user.userTag}** - \`${currency}\``;
+				})
+			].join("\n")
+		});
+	}
+};
+
+export default WalletCommand;
